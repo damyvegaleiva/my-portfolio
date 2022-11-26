552d9b4f7428dee154256da2139f1160579227b4
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-11-25 22:21:20 -0600

    Navbar responsive finished + skills slider + desktop design + animations.

--- script/script.js
+++ script/script.js
@@ -1,9 +1,26 @@
 const navbarMenu = document.querySelector(".nav-menu");
 const navbarLink = document.querySelectorAll(".nav-link");
 const hamburguerBtn = document.getElementById("hamburguer-btn");
+const navbar = document.querySelector(".nav-bar");
+const angleRight = document.getElementById("angle-right");
+const certificateWD = document.querySelector(".certificate-wd");
+const certificateJS = document.querySelector(".certificate-js");
+
+
+angleRight.addEventListener("click", () => {
+
+
+    certificateJS.classList.toggle("active");
+    certificateWD.classList.toggle("active");
+
+
+})
+
 
 hamburguerBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
+    navbar.classList.toggle("active");
+
     if (hamburguerBtn.classList.contains("active")) {
         hamburguerBtn.classList.remove("active")
     } else {
@@ -13,4 +30,6 @@ hamburguerBtn.addEventListener("click", () => {
 
 navbarLink.forEach(element => element.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
+    hamburguerBtn.classList.remove("active")
+    navbar.classList.toggle("active");
 }))
