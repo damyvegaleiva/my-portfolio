a2b9a1ec3540265bca4f960fa01895ed47764b77
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-12-06 14:38:07 -0600

    my works added.

--- script/script.js
+++ script/script.js
@@ -1,35 +1,36 @@
 const navbarMenu = document.querySelector(".nav-menu");
 const navbarLink = document.querySelectorAll(".nav-link");
 const hamburguerBtn = document.getElementById("hamburguer-btn");
-const navbar = document.querySelector(".nav-bar");
+const navBar = document.querySelector(".nav-bar");
 const angleRight = document.getElementById("angle-right");
+const angleLeft = document.getElementById("angle-left");
 const certificateWD = document.querySelector(".certificate-wd");
 const certificateJS = document.querySelector(".certificate-js");
 
 
-angleRight.addEventListener("click", () => {
+angleRight.onclick = certificate;
+angleLeft.onclick = certificate;
 
 
+function certificate() {
     certificateJS.classList.toggle("active");
     certificateWD.classList.toggle("active");
-
-
-})
+}
 
 
 hamburguerBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
-    navbar.classList.toggle("active");
+    navBar.classList.toggle("active");
 
     if (hamburguerBtn.classList.contains("active")) {
-        hamburguerBtn.classList.remove("active")
+        hamburguerBtn.classList.remove("active");
     } else {
-        hamburguerBtn.classList.add("active")
+        hamburguerBtn.classList.add("active");
     }
-})
+});
 
 navbarLink.forEach(element => element.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
-    hamburguerBtn.classList.remove("active")
-    navbar.classList.toggle("active");
-}))
+    hamburguerBtn.classList.remove("active");
+    navBar.classList.toggle("active");
+}));
