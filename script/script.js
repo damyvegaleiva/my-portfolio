8f9b0990e999bccc0ce244091c345b8946de0cb0
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-12-06 16:52:35 -0600

    Carousel certificates.

--- script/script.js
+++ script/script.js
@@ -7,16 +7,15 @@ const angleLeft = document.getElementById("angle-left");
 const certificateWD = document.querySelector(".certificate-wd");
 const certificateJS = document.querySelector(".certificate-js");
 
-
 angleRight.onclick = certificate;
 angleLeft.onclick = certificate;
-
 
 function certificate() {
     certificateJS.classList.toggle("active");
     certificateWD.classList.toggle("active");
+    document.querySelector(".fa-angle-left").classList.toggle("hidden");
+    document.querySelector(".fa-angle-right").classList.toggle("hidden");
 }
-
 
 hamburguerBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("active");
